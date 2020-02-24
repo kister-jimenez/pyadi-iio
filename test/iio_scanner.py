12f50677275fb97aef0fc7abc2a7d72ef16b4f8c
Trecia Agoylo <trecia.agoylo@analog.com>
2020-02-24 14:25:57 +0800

    Added parametrized test for ad9364
    parametrized test for ad9364 (fmcomms4 and adrv9364)

--- test/iio_scanner.py
+++ test/iio_scanner.py
@@ -13,6 +13,7 @@ HOSTNAMES = [
     "analog-zc706-daq2.local",
     "analog-zc706-fmcomms2.local",
     "analog-zc706-daq3.local",
+    "analog-adrv9364.local",
 ]
 
 
@@ -81,9 +82,7 @@ def check_board_other(ctx):
         ctx, [device("ad9517"), device("ad9361-phy"), device("cf-ad9361-lpc", 4)]
     ):
         return "adrv9361"
-    if check_config(
-        ctx, [device("ad9517"), device("ad9361-phy"), device("cf-ad9361-lpc", 2)]
-    ):
+    if check_config(ctx, [device("ad7291-bob"), device("cf-ad9361-lpc", 2)]):
         return "adrv9364"
     if check_config(
         ctx, [device("adm1177"), device("ad9361-phy"), device("cf-ad9361-lpc", 2)]
@@ -93,6 +92,10 @@ def check_board_other(ctx):
         ctx, [device("ad7291"), device("ad9361-phy"), device("cf-ad9361-lpc", 4)]
     ):
         return "fmcomms2"
+    if check_config(
+        ctx, [device("ad7291"), device("ad9361-phy"), device("cf-ad9361-lpc", 2),],
+    ):
+        return "fmcomms4"
     if check_config(ctx, [device("ad9361-phy"), device("ad9361-phy-b")]):
         return "fmcomms5"
     if check_config(ctx, [device("ad9361-phy"), device("cf-ad9361-lpc", 2)]):
