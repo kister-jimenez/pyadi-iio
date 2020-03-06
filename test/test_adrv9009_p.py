744fa59bb6690c04b7f53c05f1973925c025b141
Trecia Agoylo <trecia.agoylo@analog.com>
2020-03-06 15:59:01 +0800

    added adrv9009 parametrized test

--- test/test_adrv9009_p.py
+++ test/test_adrv9009_p.py
@@ -21,10 +21,10 @@ def test_adrv9009_attr(
 
 
 #########################################
-@pytest.mark.parametrize("classname, hardware", [(classname, hardware)])
-@pytest.mark.parametrize("channel", [0, 1]
-def test_adrv9009_rx_data(test_dma_rx, classname, hardware, channel):
-    test_dma_rx(classname, hardware, channel)
+#@pytest.mark.parametrize("classname, hardware", [(classname, hardware)])
+#@pytest.mark.parametrize("channel", [0, 1]
+#def test_adrv9009_rx_data(test_dma_rx, classname, hardware, channel):
+#    test_dma_rx(classname, hardware, channel)
 
 
 #########################################
